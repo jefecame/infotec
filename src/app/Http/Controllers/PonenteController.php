c66f9034c9efb16bf448717fe0d2186c9751858e
Jesús Fernando Carrasco Mendoza <jefecame@hotmail.com>
2025-09-16 12:19:16 -0700

    Ponente y Asistente

---
+++ src/app/Http/Controllers/PonenteController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ponente;
+use Illuminate\Http\Request;
+
+class PonenteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Ponente $ponente)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Ponente $ponente)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Ponente $ponente)
+    {
+        //
+    }
+}
